474930147d83873615b6b9c0024848d0b33d6332
Zachary Klein <zak@silver-chalice.com>
2016-11-29 23:00:34 -0500

    added react sample app

---
+++ sampleApps/react/grails-app/controllers/react/ApplicationController.groovy
@@ -0,0 +1,17 @@
+package react
+
+import grails.core.GrailsApplication
+import grails.util.Environment
+import grails.plugins.*
+
+class ApplicationController implements PluginManagerAware {
+
+    GrailsApplication grailsApplication
+    GrailsPluginManager pluginManager
+
+    def index() {
+        [grailsApplication: grailsApplication, pluginManager: pluginManager]
+    }
+	
+	def books() {}
+}
